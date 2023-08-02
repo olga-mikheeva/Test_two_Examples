c8185d140da44239891ca3ea14143ba87a887e7e
Olga <workpost.mkh@yandex.ru>
2023-08-02 15:01:30 +0300

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿// Особое приветствие для определенного пользователя
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
+
